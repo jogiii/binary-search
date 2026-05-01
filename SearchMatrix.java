1042d18388209839bb7a1cd50d533bb55497e675
Joginder Kumar <joginder.kumar1991@gmail.com>
2026-05-01 11:14:45 +0300

    search 2d matrix

---
+++ SearchMatrix.java
@@ -0,0 +1,28 @@
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left =0;
+        int right = (rows * cols) -1;
+
+        while(left <= right){
+            int mid = left +(right -left)/2;
+            int midValue = matrix[mid/cols][mid % cols];
+
+            if(midValue== target){
+                return true;
+            }else if(midValue > target){
+                right = midValue-1;
+            }else{
+                left = midValue+1;
+            }
+
+
+
+
+        }
+        return false;
+    }
+    
+    
+}
